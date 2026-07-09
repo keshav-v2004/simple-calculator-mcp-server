c20c1ae5bd579c50c978014f580374ae19acfd21
keshav-v2004 <keshavcse.ucs1536@gmail.com>
2026-07-09 18:25:13 +0530

    setup for deployement on render

--- main.py
+++ main.py
@@ -172,6 +172,13 @@ async def get_history_and_memory() -> dict:
     }
 
 if __name__ == '__main__':
-    mcp.run(transport='http', host='0.0.0.0', port=8000)
+    
+    import os
+    
+
+    port = int(os.environ.get("PORT", 8000))
+    
+
+    mcp.run(transport='sse', host='0.0.0.0', port=port)
 
 
